594df57504a65b1f4f43226998b637b2c534c871
SauryANever23 <120295720+SauryANever23@users.noreply.github.com>
2024-11-12 20:18:25 +0545

    Adding the first main files in the repo

---
+++ main-cli.py
@@ -0,0 +1,8 @@
+
+class Finance():
+
+    def menu_screen() -> None:
+        """
+        This functino will display the menu screen at the begginig of the application:
+        this will ajorly be something of no 
+        """
